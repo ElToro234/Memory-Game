4d41d9260e6b87ef08d58ef2741d51903123a6bc
Alexander Liu <alexanderliuxinlu@gmail.com>
2024-09-16 15:50:34 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import SingleCard from './components/SingleCard'
 
 const cardImages = [
   { "src": "/img/IMG_1.jpeg"},
@@ -13,6 +14,8 @@ const cardImages = [
 function App() {
   const[cards, setCards] = useState([])
   const[turns, setTurns] = useState(0)
+  const[choiceOne, setChoiceOne] = useState(null);
+  const[choiceTwo, setChoiceTwo] = useState(null);
 
   //Shuffle the cards
   const shuffleCards = () => {
@@ -23,18 +26,35 @@ function App() {
     setCards(shuffleCards)
     setTurns(0)
   }
-    console.log(cards, turns)
 
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect (() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log ("MATCH")
+        resetTurn()
+      }else{
+        console.log("not match")
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
   return (
     <div className="App">
       <h1>Memory Game</h1>
       <button onClick={ shuffleCards }>New Game</button>
       <div className = "card-grid" >
         {cards.map (card => (
-          <div className = "card" key = {card.id} > 
-            <img className="front" src = {card.src} alt = "front card"/>
-            <img className="back" src = "/img/cover.png" alt = "back card"/>
-          </div>
+          <SingleCard key = {card.id} card = { card } handleChoice = {handleChoice} />
         ))}
       </div>
     </div>
---
+++ src/components/SingleCard.js
@@ -0,0 +1,17 @@
+import './SingleCard.css'
+
+export default function SingleCard( { card, handleChoice }) {
+
+    const handleClick = () => {
+        handleChoice (card)
+    }
+
+    return (
+        <div className = "card" key = { card.id }>
+            <div > 
+            <img className="front" src = {card.src} alt = "front card"/>
+            <img className="back" onClick = {handleClick} src = "/img/cover.png" alt = "back card"/>
+          </div>
+        </div>
+    )
+}
